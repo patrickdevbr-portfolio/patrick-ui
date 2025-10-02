c2f6ca24ccb2787089e013ed3f65a3c39d2e5361
patrickdevbr <patrickribeiro.rb@gmail.com>
2025-10-02 01:57:54 -0300

    basic input css
    	modified:   examples/vue/src/App.vue
    	modified:   packages/core/src/button/button.css.ts
    	new file:   packages/core/src/field/field.css.ts
    	modified:   packages/vue/package.json
    	new file:   packages/vue/src/components/input/Input.vue
    	modified:   packages/vue/src/index.ts
    	modified:   pnpm-lock.yaml

--- packages/core/src/button/button.css.ts
+++ packages/core/src/button/button.css.ts
@@ -3,24 +3,38 @@ import { recipe } from "@vanilla-extract/recipes";
 
 export const button = recipe({
   base: {
-    padding: "8px 16px",
-    backgroundColor: "#fff",
-    backgroundImage:
-      "linear-gradient(-180deg, rgba(255, 255, 255, 0.09) 0%, rgba(17, 17, 17, 0.04) 100%)",
+    display: "inline-flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "0.5rem",
+    cursor: "pointer",
+    userSelect: "none",
+    WebkitTapHighlightColor: "transparent",
+    transition: "transform .08s ease, box-shadow .12s ease, background-color .12s ease",
+    borderRadius: ".375rem",
+    background: "linear-gradient(-180deg, rgba(255, 255, 255, 0.09) 0%, rgba(17, 17, 17, 0.04) 100%)",
     border: "1px solid rgba(22, 22, 22, 0.4)",
-    color: "#000",
+    padding: "1rem",
+    boxShadow: "0 1px 2px rgba(0, 0, 0, 0.05)",
+
 
     selectors: {
       "&:hover": {
-        backgroundImage:
-          "linear-gradient(-180deg, rgba(255, 255, 255, 0.18) 0%, rgba(17, 17, 17, 0.08) 100%)",
-        border: "1px solid rgba(22, 22, 22, 0.4)",
-        color: "#111",
+        background: "linear-gradient(-180deg, rgba(255, 255, 255, 0.18) 0%, rgba(17, 17, 17, 0.08) 100%)"
       },
+      "&:active": {
+        transform: "translateY(0)",
+        filter: "brightness(.96)"
+      },
+      "&:focus": {
+        outline: "none",
+      },
+
       "&:disabled": {
-        backgroundColor: "#a5b4fc",
-        cursor: "not-allowed",
-      },
+        color: "rgba(0, 0, 0, 0.1)",
+        backgroundColor: "rgba(0, 0, 0, 0.025)",
+        borderColor: "rgb(232, 232, 232)"
+      }
     },
   },
 
---
+++ packages/vue/src/components/input/Input.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import * as styles from '@patrick-ui/core/field/field.css'
+
+const className = computed(() => styles.input())
+</script>
+
+<template>
+    <input :class="className" />
+</template>
